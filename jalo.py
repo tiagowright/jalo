eb225d88ba26a5b905960dc2e49fb11e8c5752f6
Tiago Wright <tiagowright@gmail.com>
2025-12-13 17:41:02 -0800

    `generate` working with keyboard and pins

--- jalo.py
+++ jalo.py
@@ -281,34 +281,71 @@ class JaloShell(cmd.Cmd):
 
     commands["generate"] = Command(
         name="generate",
-        description="Generates new layouts from scratch, starting from a number of random seeds and improving them to find the best (lowest) score, based on the current objective (see `help objective`).",
+        description="Generates new layouts from scratch, starting from a number of random seeds and improving them to find the best (lowest) score, "
+            "based on the current objective (see `help objective`). "
+            "If a keyboard layout is provided, it will be used to determine the hardware and to for the location of pinned characters. " 
+            "If no keyboard layout is provided, default hardware is used and pinned characters are ignored with a warning.",
         arguments=(
-            CommandArgument("seeds", "[seeds=100]", "the number of random seeds to generate, default is 100."),
+            CommandArgument("seeds", "[seeds=100]", "the number of random seeds to generate, default is 100. Use 1000 for great results."),
+            CommandArgument("keyboard", "[<keyboard>]", "the keyboard layout to improve, can be a layout name or the index of a layout in memory."),
         ),
-        examples=("", "1000"),
+        examples=("", "1000", "100 hdpm"),
         category="editing",
         short_description="generates a wide variety of new layouts from scratch",
     )
+
+    def complete_generate(self, text: str, line: str, begidx: int, endidx: int) -> list[str]: # pyright: ignore[reportUnusedParameter]
+        arg_num = self._arg_num_at_index(line, begidx, endidx)
+        if arg_num is None or arg_num <= 1:
+            seed_suggestions = ['10', '20', '100', '1000']
+            return [suggestion for suggestion in seed_suggestions if suggestion.startswith(text)]
+
+        return self._list_keyboard_names(text)
+
 
     def do_generate(self, arg: str) -> None:
         args = self._split_args(arg)
 
         seeds = 100
+        layout = None
 
         if len(args) > 0:
             try:
                 seeds = int(args[0])
             except ValueError:
-                self._warn("seeds must be an integer: generate [seeds=100]")
+                self._warn("seeds must be an integer: generate [seeds=100] [keyboard]")
                 return
         
+        if len(args) > 1:
+            layouts = self._parse_keyboard_names(args[1])
+            if layouts is None or len(layouts) != 1:
+                self._warn("usage: generate <seeds> [keyboard]")
+                return
+            layout = layouts[0]
+
+        if layout is not None:
+            model = self._get_model(layout)
+            pinned_positions = model.pinned_positions_from_layout(layout, self.pinned_chars)
+        else:
+            model = self.model
+            pinned_positions = ()
+            if self.pinned_chars:
+                self._warn(f"no keyboard layout provided, cannot pin characters to a position, pins will be ignored ({', '.join(self.pinned_chars)})")
+
         self._info(f"generating {seeds} seeds.")
 
-        N = len(self.model.hardware.positions)
-        char_seq = self.freqdist.char_seq[:N]
+        optimizer = Optimizer(model, population_size=100)
 
-        optimizer = Optimizer(self.model, population_size=100)
-        optimizer.generate(char_seq=char_seq, seeds=seeds)
+        if layout is not None:
+            optimizer.generate(
+                seeds=seeds, 
+                char_at_pos=tuple(model.char_at_positions_from_layout(layout)), 
+                pinned_positions=pinned_positions
+            )
+        else:            
+            N = len(model.hardware.positions)
+            char_seq = self.freqdist.char_seq[:N]
+            optimizer.generate(char_seq=char_seq, seeds=seeds, pinned_positions=pinned_positions)
         
         list_num = self._layout_memory_from_optimizer(optimizer, push_to_stack=False)
 
